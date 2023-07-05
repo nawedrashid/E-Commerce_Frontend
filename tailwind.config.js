69996c6d159bd7ae2ba9de5101ed7d740807a5b2
Nawed Rashid <75127419+nawedrashid@users.noreply.github.com>
2023-07-05 10:45:36 +0530

    Initial Setup done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary:'#FBCEB1',
+      secondary:'#65000B',
+      white:'white',
+      black:'black',
+      transparent:'transparent'
+    },
+    fontFamily: {
+      logoF: ['Kaushan Script']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
