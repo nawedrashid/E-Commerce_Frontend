b387797aee4a7454f93f59ae01a3adc5a8d81265
Nawed Rashid <75127419+nawedrashid@users.noreply.github.com>
2023-08-09 03:24:43 +0530

    Order Page done

--- src/Pages/Orders.tsx
+++ src/Pages/Orders.tsx
@@ -14,6 +14,7 @@ const orderItems = [
     dPrice: 3499,
     rating: 4.2,
     category: "Chair",
+    status: "Delivered",
   },
   {
     id: 1,
@@ -27,6 +28,7 @@ const orderItems = [
     dPrice: 3499,
     rating: 4.2,
     category: "Chair",
+    status: "Packed",
   },
   {
     id: 1,
@@ -40,6 +42,7 @@ const orderItems = [
     dPrice: 3499,
     rating: 4.2,
     category: "Chair",
+    status: "Shipped",
   },
 ];
 
@@ -47,11 +50,26 @@ const Orders: React.FC = () => {
   return (
     <div className="h-auto min-h-screen px-6 w-full pt-20">
       <div className="max-w-screen-lg text-secondary mx-auto p-4">
-        <h1 className="text-2xl py-2 font-bold">Orders</h1>
+        <h1 className="text-3xl pb-4 font-bold">Orders</h1>
         <div className="flex flex-col gap-4">
-            {orderItems.map((item)=>(
-               <div></div>
-            ))} 
+          {orderItems.map((item) => (
+            <div className="bg-primary p-4 rounded-md flex gap-8">
+              <div className=" bg-bgrd rounded-md">
+                {item.child}
+              </div>
+              <div className="flex justify-between items-center w-full">
+                <div className="flex flex-col gap-2">
+                  <div className="text-2xl font-bold">{item.name}</div>
+                  <div className="font-semibold">{item.status}</div>
+                  <div>₹ {item.dPrice}</div>
+                  <div>Category: {item.category}</div>
+                </div>
+                <div className="px-4 rounded-lg my-auto py-10 text-primary bg-secondary">
+                  {">"}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
